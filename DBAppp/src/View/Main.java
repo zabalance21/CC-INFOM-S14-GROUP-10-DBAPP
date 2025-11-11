cb09523fc3c24465a06706a767f8f6830827c135
Mistery <78303181+IMisteryl@users.noreply.github.com>
2025-11-12 01:53:51 +0800

    Added GUI
    commented out current main logic using terminal/console interface, and switched it to use GUI

--- DBAppp/src/View/Main.java
+++ DBAppp/src/View/Main.java
@@ -1,17 +1,26 @@
 package View;
 
-import Controller.BranchController;
-import Controller.ClientController;
-import Controller.ManagerController;
+import GUI.ManagementGUI;
 
-import java.util.Scanner;
+//import java.util.Scanner;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        App app = new App(scanner);
-        app.mainApp();
+        // Scanner scanner = new Scanner(System.in);
+        // App app = new App(scanner);
+        // app.mainApp();
+
+        SwingUtilities.invokeLater(() -> {
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+            new ManagementGUI();
+        });
     }
 }
